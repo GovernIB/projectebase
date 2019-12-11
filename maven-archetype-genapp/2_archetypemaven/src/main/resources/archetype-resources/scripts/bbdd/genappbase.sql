114378d4c833ad017e52a56173f68740541ebf33
anadal-fundaciobit <anadal@fundaciobit.org>
2019-12-11 08:59:12 +0100

    Estructura Maven Archetype de Projecte Base #15

--- maven-archetype-genapp/2_archetypemaven/src/main/resources/archetype-resources/scripts/bbdd/genappbase.sql
+++ maven-archetype-genapp/2_archetypemaven/src/main/resources/archetype-resources/scripts/bbdd/genappbase.sql
@@ -1,20 +1,3 @@
---
--- PostgreSQL database dump
---
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = off;
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-SET escape_string_warning = off;
-
-SET search_path = ${artifactId}, pg_catalog;
-
-SET default_tablespace = '';
-
-SET default_with_oids = false;
 
 --
 -- TOC entry 150 (class 1259 OID 69544)
@@ -156,11 +139,4 @@ CREATE INDEX ${prefix}_traducmap_tradmapid_pk_i ON ${prefix}_traducciomap USING 
 
 ALTER TABLE ONLY ${prefix}_traducciomap
     ADD CONSTRAINT ${prefix}_traducmap_traduccio_fk FOREIGN KEY (traducciomapid) REFERENCES ${prefix}_traduccio(traduccioid);
-
-
--- Completed on 2014-08-25 12:39:21
-
---
--- PostgreSQL database dump complete
---
 
