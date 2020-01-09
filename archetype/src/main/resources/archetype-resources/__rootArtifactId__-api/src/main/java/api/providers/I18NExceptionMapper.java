11596a41cdcdc17219dba6be836d19b63605cad1
Antoni Reus <areus@fundaciobit.org>
2020-01-09 14:28:36 +0100

    Completar capa API/REST #23

--- archetype/src/main/resources/archetype-resources/__rootArtifactId__-api/src/main/java/api/providers/I18NExceptionMapper.java
+++ archetype/src/main/resources/archetype-resources/__rootArtifactId__-api/src/main/java/api/providers/I18NExceptionMapper.java
@@ -3,14 +3,23 @@
 #set( $symbol_escape = '\' )
 package ${package}.api.providers;
 
+import ${package}.api.utils.I18NTranslatorRest;
 import ${package}.commons.i18n.I18NException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.ejb.EJBException;
+import javax.annotation.PostConstruct;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Permet mapejar a una respota comuna a les excepcions de tipus I18NException.
@@ -22,10 +31,49 @@ public class I18NExceptionMapper implements ExceptionMapper<I18NException> {
 
     private static final Logger LOG = LoggerFactory.getLogger(I18NExceptionMapper.class);
 
+    private List<Locale> supportedLocales;
+
+    @Context
+    private HttpHeaders headers;
+
+    @Context
+    private HttpServletRequest request;
+
+    @Context
+    private ServletContext servletContext;
+
+    @PostConstruct
+    private void init() {
+        String supportedLocalesParam = servletContext.getInitParameter("supportedLocales");
+        supportedLocales = Stream.of(supportedLocalesParam.split(","))
+                .map(String::trim)
+                .map(Locale::forLanguageTag)
+                .collect(Collectors.toList());
+    }
+
     @Override
     public Response toResponse(I18NException e) {
         LOG.error("Rebuda una I18NException de la capa EJB: " + e.getMessage());
-        LOG.error("Retornam un codi 500 al client");
-        return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+
+        Locale locale = supportedLocales.isEmpty() ? Locale.getDefault() : supportedLocales.get(0);
+        String lang = request.getParameter("lang");
+        if (lang != null && !lang.isEmpty()) {
+            Locale langLocale = Locale.forLanguageTag(lang);
+            if (supportedLocales.contains(langLocale)) {
+                locale = langLocale;
+            }
+        } else {
+            List<Locale> acceptableLangauges = headers.getAcceptableLanguages();
+            for (Locale acceptableLocale : acceptableLangauges) {
+                if (supportedLocales.contains(acceptableLocale)) {
+                    locale = acceptableLocale;
+                    break;
+                }
+            }
+        }
+        LOG.error("Emprant locale: " + locale);
+
+        String msg = I18NTranslatorRest.tradueix(e, locale);
+        return Response.status(Response.Status.BAD_REQUEST).entity(msg).build();
     }
 }
