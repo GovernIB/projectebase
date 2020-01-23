0915ef77005c06625c2949466250a063e8bb4026
Antoni Reus <areus@fundaciobit.org>
2020-01-23 15:26:35 +0100

    Utilizar componentes Primefaces en las páginas xhtml #45

--- projecteorigen/projectebase-back/src/main/java/es/caib/projectebase/back/controller/ListUnitatOrganicaController.java
+++ projecteorigen/projectebase-back/src/main/java/es/caib/projectebase/back/controller/ListUnitatOrganicaController.java
@@ -1,10 +1,10 @@
 package es.caib.projectebase.back.controller;
 
 import es.caib.projectebase.commons.i18n.I18NException;
-import es.caib.projectebase.commons.utils.Configuration;
 import es.caib.projectebase.ejb.UnitatOrganicaService;
-import es.caib.projectebase.ejb.utils.I18NTranslatorEjb;
 import es.caib.projectebase.persistence.UnitatOrganica;
+import org.primefaces.model.LazyDataModel;
+import org.primefaces.model.SortOrder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,7 +16,8 @@ import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
-import java.util.Locale;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Controlador pels llistats d'Unitats Organiques. El definim a l'scope de view perquè a nivell de request es
@@ -38,22 +39,13 @@ public class ListUnitatOrganicaController implements Serializable {
 
     // PROPIETATS + GETTERS/SETTERS
 
-    /** Objecte que manté l'estat i les dades paginades */
-    private PaginationHelper<UnitatOrganica> pagination;
+    /**
+     * Model de dades emprat pel compoment dataTable de primefaces.
+     */
+    private LazyDataModel<UnitatOrganica> lazyModel;
 
-    /** Cadena de cerca */
-    private String cerca;
-
-    public PaginationHelper<UnitatOrganica> getPagination() {
-        return pagination;
-    }
-
-    public String getCerca() {
-        return cerca;
-    }
-
-    public void setCerca(String cerca) {
-        this.cerca = cerca;
+    public LazyDataModel<UnitatOrganica> getLazyModel() {
+        return lazyModel;
     }
 
     /**
@@ -62,23 +54,23 @@ public class ListUnitatOrganicaController implements Serializable {
     @PostConstruct
     public void init() {
         log.debug("init");
-        /*
-        Crea un objecte paginació implementant el mètode per actualitzar el model perquè agafi les dades
-         filtrades i paginades.
-         */
-        pagination = new PaginationHelper<>() {
+
+        lazyModel = new LazyDataModel<>() {
+            /*
+            Primefaces cridarà automàticament aquest mètode quan necessita actualitzar les dades del dataTable
+            per qualsevol circumstància (filtres, ordenació, canvi de pàgina ...)
+            */
             @Override
-            public void updateModel() {
-                setCount((int) unitatOrganicaService.countFilter(cerca));
-                //TODO implementar ordenació
+            public List<UnitatOrganica> load(int first, int pageSize, String sortField, SortOrder sortOrder,
+                                             Map<String, Object> filters) {
                 try {
-                    setModel(unitatOrganicaService.selectAll(cerca, this.getPageFirstItem(), this.getPageSize()));
+                    // Es necessari indicar el nombre de registres cada vegada que es carrega el model per si ha variat
+                    setRowCount((int) unitatOrganicaService.countAll());
+                    //TODO implementar ordenació i filtres
+                    return unitatOrganicaService.selectAll(first, pageSize);
                 } catch (I18NException e) {
-                    String msg = I18NTranslatorEjb.translate(e, new Locale(Configuration.getDefaultLanguage()));
-                    log.error("Error en updateModel() => " + msg);
-                    throw new RuntimeException(msg);
+                    throw new RuntimeException(e);
                 }
-                
             }
         };
     }
@@ -93,8 +85,6 @@ public class ListUnitatOrganicaController implements Serializable {
     public void delete(Long id) {
         log.debug("delete");
         unitatOrganicaService.deleteById(id);
-        // Actualitza les dades paginades
-        pagination.updateModel();
-        context.addMessage(null, new FacesMessage("Registre borrat"));
+        context.addMessage(null, new FacesMessage("Registre borrat", ""));
     }
 }
