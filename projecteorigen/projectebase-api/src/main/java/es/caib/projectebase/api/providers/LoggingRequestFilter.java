c13e3ea2a3728960959244dfbbd3a7cef3b65e7d
Antoni Reus <areus@fundaciobit.org>
2020-02-05 10:19:46 +0100

    millores api REST

--- projecteorigen/projectebase-api/src/main/java/es/caib/projectebase/api/providers/LoggingRequestFilter.java
+++ projecteorigen/projectebase-api/src/main/java/es/caib/projectebase/api/providers/LoggingRequestFilter.java
@@ -3,17 +3,27 @@ package es.caib.projectebase.api.providers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.Priority;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.container.PreMatching;
 import javax.ws.rs.ext.Provider;
 
 /**
- * Realitza un log de totes les peticions rebudes.
+ * Realitza un log de totes les peticions rebudes. Es configura com amb {@link PreMatching} perquè la petició
+ * encara que no es correspongui amb cap resource. Sinó només és loguejarien les petcions una vegada ja s'ha determinat
+ * a quin resource pertanyen.
+ * Els filtres s'ordenen per prioritat ascendent a l'entrada i prioritat descendent a la sortida. Això assegura que
+ * a l'entrada sigui el primer i a la sortida el darrer.
  *
  * @author areus
  */
 @Provider
-public class LoggingRequestFilter implements ContainerRequestFilter {
+@PreMatching
+@Priority(100)
+public class LoggingRequestFilter implements ContainerRequestFilter , ContainerResponseFilter {
 
     private static final Logger LOG = LoggerFactory.getLogger(LoggingRequestFilter.class);
 
@@ -23,6 +33,17 @@ public class LoggingRequestFilter implements ContainerRequestFilter {
      */
     @Override
     public void filter(ContainerRequestContext request) {
-        LOG.debug("Rest call: " + request.getMethod() + " " + request.getUriInfo().getRequestUri().toString());
+        LOG.debug("Request: " + request.getMethod() + " " + request.getUriInfo().getRequestUri());
+    }
+
+    /**
+     * Realitza un log del mètode HTTP i el request URI amb l'status de la resposta.
+     * @param request informació de context de la petició
+     * @param response informació de context de la resposta
+     */
+    @Override
+    public void filter(ContainerRequestContext request, ContainerResponseContext response) {
+        LOG.debug("Response: " + request.getMethod() + " " + request.getUriInfo().getRequestUri()
+                + " = " + response.getStatus());
     }
 }
