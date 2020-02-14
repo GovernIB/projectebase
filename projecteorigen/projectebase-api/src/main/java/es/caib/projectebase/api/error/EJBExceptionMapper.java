94a87b6f31da5b2a929faf418433c10420d193a4
Antoni Reus <areus@fundaciobit.org>
2020-02-14 12:25:57 +0100

    canviar RuntimeException a EJBException ja que si capturam qualsevol Runtime ens menjam les excepcions pròpies de JaxRS com un NotFound que ha de donar un 404 i no un 500

--- projecteorigen/projectebase-api/src/main/java/es/caib/projectebase/api/error/EJBExceptionMapper.java
+++ projecteorigen/projectebase-api/src/main/java/es/caib/projectebase/api/error/EJBExceptionMapper.java
@@ -9,20 +9,20 @@ import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
 /**
- * Permet mapejar a una respota qualsevol excepció unchecked.
+ * Permet mapejar a una respota qualsevol excepció unchecked de la capa EJB.
  * Envia un codi d'error 500 i evita que els detalls de l'excepció arribin al client,
  * ja que un error de sistema l'ha de mirar l'administrador.
  *
  * @author areus
  */
 @Provider
-public class RuntimeExceptionMapper implements ExceptionMapper<RuntimeException> {
+public class EJBExceptionMapper implements ExceptionMapper<EJBException> {
 
-    private static final Logger LOG = LoggerFactory.getLogger(RuntimeExceptionMapper.class);
+    private static final Logger LOG = LoggerFactory.getLogger(EJBExceptionMapper.class);
 
     @Override
-    public Response toResponse(RuntimeException e) {
-        LOG.error("Rebuda una RuntimeException: " + e.getMessage());
+    public Response toResponse(EJBException e) {
+        LOG.error("Rebuda una EJBException: " + e.getMessage());
         LOG.error("Retornam un codi 500 al client");
         return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
     }
