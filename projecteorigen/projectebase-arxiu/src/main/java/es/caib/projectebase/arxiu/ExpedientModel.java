318adb18404c087aebd8487422e6c7986e8cca7d
Antoni Reus <areus@fundaciobit.org>
2020-03-17 15:46:16 +0100

    esquelet de mòdul d'exemple amb arxiu. creació d'expedients #42

--- projecteorigen/projectebase-arxiu/src/main/java/es/caib/projectebase/arxiu/ExpedientModel.java
+++ projecteorigen/projectebase-arxiu/src/main/java/es/caib/projectebase/arxiu/ExpedientModel.java
@@ -1,11 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package es.caib.projectebase.arxiu;
 
 import javax.enterprise.inject.Model;
+import javax.validation.constraints.NotEmpty;
 
 /**
  *
@@ -14,6 +10,7 @@ import javax.enterprise.inject.Model;
 @Model
 public class ExpedientModel {
     
+    @NotEmpty
     private String name;
 
     public String getName() {
