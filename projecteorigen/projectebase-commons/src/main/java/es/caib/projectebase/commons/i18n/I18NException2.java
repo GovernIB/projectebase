fe1d9d40e58f6d157634125e28cebcc93843eb6f
Antoni Reus <areus@fundaciobit.org>
2020-04-23 10:41:01 +0200

    afegir noves classes i18n i test #29

---
+++ projecteorigen/projectebase-commons/src/main/java/es/caib/projectebase/commons/i18n/I18NException2.java
@@ -0,0 +1,79 @@
+package es.caib.projectebase.commons.i18n;
+
+import java.util.Locale;
+
+/**
+ * Excepció que permet emprar missatges traduïbles.
+ * Emprarà una instància de {@link I18NTranslator2} per traduir els missatges.
+ * Les subclasses han d'implementar el mètode {@link #getTranslator()} per indicar
+ * la instància a emprar per traduir els missatges.
+ */
+public abstract class I18NException2 extends Exception {
+
+    private static final long serialVersionUID = -3124602547590507219L;
+
+    private Object[] parameters;
+
+    /**
+     * Crea una excepció amb un missatge traduïble.
+     * @param message etiqueta del missatge a traduir.
+     */
+    public I18NException2(String message) {
+        super(message);
+    }
+
+    /**
+     * Crea una excepció amb un missatge traduïble i els paràmetres indicats.
+     * @param message etiqueta del missatge a traduir.
+     * @param parameters paràmetres del missatge.
+     */
+    public I18NException2(String message, Object... parameters) {
+        super(message);
+        this.parameters = parameters;
+    }
+
+    /**
+     * Crea una excepció amb un missatge traduïble.
+     * @param cause excepció original
+     * @param message etiqueta del missatge a traduir.
+     */
+    public I18NException2(Throwable cause, String message) {
+        super(message, cause);
+    }
+
+    /**
+     * Crea una excepció amb un missatge traduïble i els paràmetres indicats.
+     * @param cause excepció original
+     * @param message etiqueta del missatge a traduir.
+     * @param parameters paràmetres del missatge.
+     */
+    public I18NException2(Throwable cause, String message, Object... parameters) {
+        super(message, cause);
+        this.parameters = parameters;
+    }
+
+    /**
+     * Proporciona la instància de {@link I18NTranslator2} que s'emprarà per
+     * traduir els missatges.
+     * @return instància per traduir els missatges.
+     */
+    protected abstract I18NTranslator2 getTranslator();
+
+    /**
+     * Retorna el missatge de l'excepció traduit en l'idioma per defecte, {@link Locale#getDefault()}.
+     * @return missatge traduit.
+     */
+    @Override
+    public String getLocalizedMessage() {
+        return getLocalizedMessage(Locale.getDefault());
+    }
+
+    /**
+     * Retorna el missatge de l'excepció traduit en l'idioma indicat.
+     * @param locale idioma per traduir.
+     * @return missatge traduit.
+     */
+    public String getLocalizedMessage(Locale locale) {
+        return getTranslator().translate(locale, super.getMessage(), parameters);
+    }
+}
--- projecteorigen/projectebase-commons/src/main/java/es/caib/projectebase/commons/i18n/MultipleResourceBundle.java
+++ projecteorigen/projectebase-commons/src/main/java/es/caib/projectebase/commons/i18n/MultipleResourceBundle.java
@@ -1,10 +1,8 @@
 package es.caib.projectebase.commons.i18n;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.ResourceBundle;
 import java.util.stream.Collectors;
@@ -14,30 +12,49 @@ import java.util.stream.Collectors;
  * de bundles.
  *
  * @author anadal
+ * @author areus
  */
 public class MultipleResourceBundle extends ResourceBundle {
 
     private final List<ResourceBundle> delegates;
 
+    /**
+     * Construeix un nou ResourceBundle que delega en la llista de ResourceBundle indicats.
+     * @param resourceBundles llista de ResourceBundle delegats. Ha de contenir al manco un element.
+     */
     public MultipleResourceBundle(List<ResourceBundle> resourceBundles) {
-        this.delegates = resourceBundles == null ? new ArrayList<>() : resourceBundles;
+        if (resourceBundles == null || resourceBundles.isEmpty()) {
+            throw new IllegalArgumentException("resourceBundles ha de contenir al manco un element");
+        }
+        this.delegates = resourceBundles;
     }
 
+    /**
+     * Retorna la primera aparició de la clau dins els ResourceBundle delegats. Mira per ordre
+     * en el que varen ser subministrats els ResourceBundle delegats.
+     * @param key Clau a cercar
+     * @return Primera aparició de la clau o null en cas contrari.
+     */
     @Override
     protected Object handleGetObject(String key) {
         Optional<Object> firstPropertyValue = this.delegates.stream()
-                .filter(delegate -> delegate != null && delegate.containsKey(key))
+                .filter(delegate -> delegate.containsKey(key))
                 .map(delegate -> delegate.getObject(key))
                 .findFirst();
 
         return firstPropertyValue.orElse(null);
     }
 
+    /**
+     * Retorna una enumeració de totes les claus incloses dins els ResourceBundle delegats.
+     * Si hi ha claus duplicades només les retorna una vegada.
+     * @return enumeració de les claus dels resource bundle delegats.
+     */
     @Override
     public Enumeration<String> getKeys() {
         List<String> keys = this.delegates.stream()
-                .filter(Objects::nonNull)
                 .flatMap(delegate -> Collections.list(delegate.getKeys()).stream())
+                .distinct()
                 .collect(Collectors.toList());
 
         return Collections.enumeration(keys);
