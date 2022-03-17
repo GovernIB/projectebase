e4f70db9311967a751815d440f7fccc5831a43ef
Antoni Nadal <anadal@fundaciobit.org>
2022-03-17 13:17:52 +0100

    Obertura Versió #128

--- generat/projectebase.bat
+++ generat/projectebase.bat
@@ -2,7 +2,7 @@
 rmdir /Q /S projectebase
 
 set MAVEN_OPTS="-Dfile.encoding=UTF-8" && mvn org.apache.maven.plugins:maven-archetype-plugin:3.2.0:generate -B -DarchetypeGroupId=es.caib.projectebase ^
-  -DarchetypeArtifactId=projectebase-archetype -DarchetypeVersion=1.0.10 -Dpackage=es.caib.projectebase ^
+  -DarchetypeArtifactId=projectebase-archetype -DarchetypeVersion=1.0.11 -Dpackage=es.caib.projectebase ^
   -Dpackagepath=es/caib/projectebase -Dinversepackage=projectebase.caib.es -DgroupId=es.caib.projectebase ^
   -DartifactId=projectebase -Dversion=1.0.0 -Dprojectname=ProjecteBase -Dprojectnameuppercase=PROJECTEBASE ^
   -Dprefix=pbs -Dprefixuppercase=PBS -DperfilBack=true -DperfilFront=true -DperfilApiFirmaSimple=true ^
